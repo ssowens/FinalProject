155b6ed071313f09367e90c4f9ded1f687306dc9
Sheila Owens <ssowens711@gmail.com>
2018-06-02 23:29:28 -0400

    Flavors work

--- app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.ssowens.android.javajokeslib.JavaJokes;
 
 import butterknife.BindView;
@@ -38,14 +36,6 @@ public class MainActivityFragment extends Fragment {
         // Bind the views
         ButterKnife.bind(this, root);
 
-        AdView mAdView = root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
         return root;
     }
 
